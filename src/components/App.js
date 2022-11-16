a715475ff2ffdf2005ef91b881867aed0467fd6d
Tez01 <tejpartap02@gmail.com>
2022-11-15 21:33:49 -0500

    Add react routing for profile, login and signup page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
 import Signup from "./Signup";
 import { AuthenticationProvider } from "../contexts/AuthenticationContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <AuthenticationProvider>
-      <Signup />
-    </AuthenticationProvider>
+    <BrowserRouter>
+      <AuthenticationProvider>
+        <Routes>
+          {/* <Route exact path="/" component={Profile} /> */}
+          <Route exact path="/signup" element={<Signup />} />
+          {/* <Route path="/login" component={Login} /> */}
+        </Routes>
+      </AuthenticationProvider>
+    </BrowserRouter>
   );
 }
 
